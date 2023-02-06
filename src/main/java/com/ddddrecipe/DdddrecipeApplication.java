a8554f7b6604638f46d734ff38c8261b3b4d82e0
hypercubist <58693701+hypercubist@users.noreply.github.com>
2023-02-06 19:33:23 +0900

    create spring project from start.spring.io

---
+++ src/main/java/com/ddddrecipe/DdddrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.ddddrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdddrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdddrecipeApplication.class, args);
+	}
+
+}
