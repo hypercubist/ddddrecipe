b61cf0266998ad3167ed072027e4df928399cb3c
hypercubist <58693701+hypercubist@users.noreply.github.com>
2023-02-07 17:48:24 +0900

    setup h2-console load (needed csrf, xframeoptions disabled)

--- src/main/java/com/ddddrecipe/security/SecurityConfiguration.java
+++ src/main/java/com/ddddrecipe/security/SecurityConfiguration.java
@@ -4,24 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/resources/**", "/testdb");
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
+                .authorizeHttpRequests()
+                    .antMatchers("/testdb/**").permitAll()
+                    .anyRequest().authenticated()
+                .and()
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 }
